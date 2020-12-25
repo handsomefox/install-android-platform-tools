d8558b753c1aa14e5f9084693c1fcf8c67ea9224
handsomefox <handsomefox.hf@gmail.com>
2020-12-26 01:21:28 +0200

    Add and use 7zip command line

--- install.bat
+++ install.bat
@@ -1,3 +1,8 @@
-pip install elevate
-cd scripts
-install.py
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+
+cd 7za
+7za x *.zip -oc:\
+ECHO %PATH% > PATH.TMP
+SETX PATH "%PATH%;%SYSTEMDRIVE%\platform-tools" /m
+pause
